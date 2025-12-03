3991f4a8317f3fa75a99ec4a573cf8044a34c633
Alejandro Segovia <asega1996@gmail.com>
2025-12-03 08:38:25 +0100

    Structural: update Adapter usage scenarios

--- 02-structural/adapter/usage.ts
+++ 02-structural/adapter/usage.ts
@@ -1,21 +1,21 @@
 import { ApiUser } from "./types";
 
-const mockApiUserRes: ApiUser = {
+const mockApiUserResponse: ApiUser = {
     first_name: "Alejandro",
     last_name: "Segovia",
-    email: "alejandro@example.com",
-    phone_number: "+34 600 000 000",
+    email: "asega1996@gmail.com",
+    phone_number: "+34 608 842 858",
 };
 
 
 // Using OOP Adapter
 import { UserFormatterAdapter } from "./implementations/oop";
 const oopFormatter = new UserFormatterAdapter();
-const domainUserFromOop = oopFormatter.format(mockApiUserRes);
+const domainUserFromOop = oopFormatter.format(mockApiUserResponse);
 console.log("OOP adapter:", domainUserFromOop);
 
 // Using FP Adapter
 import { createUserFormatter } from "./implementations/fp";
 const fpFormatter = createUserFormatter();
-const domainUserFromFp = fpFormatter.format(mockApiUserRes);
+const domainUserFromFp = fpFormatter.format(mockApiUserResponse);
 console.log("FP adapter:", domainUserFromFp);
